d2c598b532e4a15077802fde1dd8f2d95a4df2cb
Mohsen-Bisp0017 BANAN <form@mohsen.banan.1.byname.net>
2019-12-16 08:43:39 -0800

    Starting transition to e27.

---
+++ boot/boot-27f.el
@@ -0,0 +1,31 @@
+;;; -*- Mode: Emacs-Lisp; -*-
+
+(lambda () "
+####+BEGIN: bx:dblock:bnsm:top-of-menu "basic"
+*  #Controls:   [[elisp:(toggle-read-only)][read/wr]] | [[elisp:(show-all)][Show-All]]  [[elisp:(org-shifttab)][Overview]]  [[elisp:(progn (org-shifttab) (org-content))][Content]] | [[elisp:(delete-other-windows)][(1)]] | [[elisp:(progn (save-buffer) (kill-buffer))][S&Q]]  [[elisp:(save-buffer)][Save]]  [[elisp:(kill-buffer)][Quit]] 
+####+END:
+")
+
+(message "Bystar User Env Finished Loading")
+
+;;}}}
+
+;;{{{ end of file
+
+
+(lambda () "
+#+STARTUP: showall
+"
+)
+
+
+;;}}}
+
+;;; local variables:
+;;; major-mode: emacs-lisp-mode
+;;; folded-file: nil
+;;; byte-compile-dynamic: t
+;;; end:
+
+
+
