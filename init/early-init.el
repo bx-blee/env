0aad7b341ede20bc2939c4141be230d182c1cf4a
Mohsen-Bisp0017 BANAN <form@mohsen.banan.1.byname.net>
2019-12-30 17:49:38 -0800

    Are to be used with the blee script.

---
+++ init/early-init.el
@@ -0,0 +1,8 @@
+;; Time-stamp: <2018-02-20 14:49:15 kmodi>
+;; Emacs 27.x: http://git.savannah.gnu.org/cgit/emacs.git/commit/?id=24acb31c04b4048b85311d794e600ecd7ce60d3b
+
+;; (setq package-user-dir (let ((elpa-dir-name (format "elpa_%s" emacs-major-version))) ;default = ~/.emacs.d/elpa/
+;;                          (file-name-as-directory (expand-file-name elpa-dir-name user-emacs-directory))))
+
+(setq package-user-dir (expand-file-name "/bisos/blee/27f/elpa"))
+
